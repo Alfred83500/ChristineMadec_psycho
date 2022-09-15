942d5dfc1a2ae6ec4e50903aababb237d3e8b098
Alfred83500 <85100356+Alfred83500@users.noreply.github.com>
2022-09-15 09:40:10 +0200

    Initial commit from Create Next App

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -6,24 +6,17 @@ export default function Footer() {
     return (
 
         <footer className="flex flex-row items-center justify-center ">
-            <div className='italic'>
-
-                Made by <span
-                className="font-semibold text-md text-gray-700 bg-transparent">&nbsp;Arale Agency </span> &copy; &nbsp;  2022 -
-
-
-            </div>
 
             <Link href="/mentionslegales" >
-                    <a className="italic underline"> <span className="ml-2"> Mentions légales </span></a>
+                    <a className="italic underline"> <span className="ml-2 text-sm"> Mentions légales </span></a>
                 </Link>
             <span className="ml-2">-</span>
             <Link href="/conditiongenerales">
-                    <a> <span className="ml-2 italic underline">Conditions générales de vente </span></a>
+                    <a> <span className="ml-2 italic underline text-sm">CGV </span></a>
                 </Link>
             <span className="ml-2">-</span>
             <Link href="/confidentialite">
-                    <a> <span className="ml-2 italic underline">Politique de confidentialité </span></a>
+                    <a> <span className="ml-2 italic underline text-sm">Politique de confidentialité </span></a>
                 </Link>
 
 
